5e906fd5e1d80e1afcac50ad753526a7a77d9891
saltizer <43050989+saltizer@users.noreply.github.com>
2018-09-17 11:13:07 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -18,7 +18,9 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
-node = request.RawPC("node")
+node = request.XenVM("node")
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+node.routable_control_ip = "true"
 
 # Install and execute a script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
